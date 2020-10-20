05ba5fae0630fb77ffc126b5c492c7e4d07c5b00
Vladyslav Nikitiuk <vladnik2k@gmail.com>
2020-10-20 17:06:04 +0300

    Add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {Algorithm} from './algorithm';
-import {LanguagesEnum} from './languages.enum';
 import {Settings} from './settings';
 
 @Component({
@@ -9,16 +8,19 @@ import {Settings} from './settings';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  LanguagesEnum = LanguagesEnum;
-
   settings: Settings;
 
   constructor() {
     this.settings = new Settings();
   }
 
-  click() {
+  click(): void {
     const normal = new Algorithm(this.settings);
     normal.startAlgorithm();
+  }
+
+  useResult(): void {
+    this.settings.text = this.settings.resultText;
+    this.settings.isBack = !this.settings.isBack;
   }
 }
--- src/app/languages.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum LanguagesEnum {
-  ALL = 'all',
-  EN = 'en'
-}
