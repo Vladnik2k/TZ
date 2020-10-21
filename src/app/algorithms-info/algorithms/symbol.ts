6c4cb73906364e5932161567065c724ab719baa2
Vladyslav Nikitiuk <vladnik2k@gmail.com>
2020-10-21 11:21:10 +0300

    Fix tests

--- src/app/algorithms-info/algorithms/symbol.ts
+++ src/app/algorithms-info/algorithms/symbol.ts
@@ -7,6 +7,14 @@ export class Symbol extends AlgorithmBase {
     super(settings);
   }
 
+  private static getIndex(index, additional, max): number {
+    if (additional < 0) {
+      return (index + additional + (-1 * additional * max)) % max;
+    } else {
+      return (index + additional) % max;
+    }
+  }
+
   protected divideTextForNext(text: string): void {
     this.settings.parts = new Array<Part>();
     for (const symbol of text) {
@@ -35,8 +43,8 @@ export class Symbol extends AlgorithmBase {
     for (let i = 0; i < this.settings.code.length; i++) {
       for (let j = 0; j < this.settings.code[i].length; j++) {
         if (this.settings.code[i][j].indexOf(part.before.toUpperCase()) !== -1) {
-          const ii = this.getIndex(i, additionalRowIndex, this.settings.code.length);
-          const jj = this.getIndex(j, additionalColIndex, this.settings.code[0].length);
+          const ii = Symbol.getIndex(i, additionalRowIndex, this.settings.code.length);
+          const jj = Symbol.getIndex(j, additionalColIndex, this.settings.code[0].length);
           part.result = isUpperCase ? this.settings.code[ii][jj][0].toUpperCase() : this.settings.code[ii][jj][0].toLowerCase();
           part.status = PartStatusesEnum.DONE;
           return;
@@ -45,10 +53,6 @@ export class Symbol extends AlgorithmBase {
     }
 
     part.status = PartStatusesEnum.ERROR;
-  }
-
-  private getIndex(index, additional, max): number {
-    return (index + additional + max) % max;
   }
 
 }
